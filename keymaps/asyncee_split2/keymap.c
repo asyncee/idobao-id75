52c23b483ad02b1de38254ff8256e5ece1af744b
asyncee <asyncee@users.noreply.github.com>
2022-09-26 15:39:49 +0300

    fix

--- keymaps/asyncee_split2/keymap.c
+++ keymaps/asyncee_split2/keymap.c
@@ -14,8 +14,8 @@
 #define SPC_L1 LT(1, KC_SPC)
 
 // osm
-#define OSM_ALT OSM(MOD_LALT)
-#define OSM_GUI OSM(MOD_LGUI)
+// #define OSM_ALT OSM(MOD_LALT)
+// #define OSM_GUI OSM(MOD_LGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_ortho_5x15(
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F7,   KC_F8,   KC_F9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
 	KC_LSFT, MA,      MS,      MD,      MF,      KC_G,    KC_F4,   KC_F5,   KC_F6,   KC_H,    MJ,      MK,      ML,      MSCLN,   KC_QUOT,
 	KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F1,   KC_F2,   KC_F3,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
-	KC_LCTL, KC_NO,   KC_NO,   KC_ESC,  KC_ESC,  SPC_L1,  KC_LBRC, KC_BSLS, KC_RBRC  KC_ENT,  KC_BSPC, KC_DEL,  KC_NO,   KC_NO,   KC_NO
+	KC_LCTL, KC_NO,   KC_NO,   KC_ESC,  KC_ESC,  SPC_L1,  KC_LBRC, KC_BSLS, KC_RBRC, KC_ENT,  KC_BSPC, KC_DEL,  KC_NO,   KC_NO,   KC_NO
  ),
  [1] = LAYOUT_ortho_5x15(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
